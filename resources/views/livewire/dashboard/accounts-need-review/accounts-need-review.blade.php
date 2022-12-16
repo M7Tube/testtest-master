6101664384b188a272a6104f30a385d53779251c
M7Tube <clashroyale.mahh@gmail.com>
2022-12-15 22:58:04 -0800

    ؤؤ

--- resources/views/livewire/dashboard/accounts-need-review/accounts-need-review.blade.php
+++ resources/views/livewire/dashboard/accounts-need-review/accounts-need-review.blade.php
@@ -66,7 +66,7 @@
                                                         <div class="modal-content">
                                                             <div class="modal-header">
                                                                 <h1 class="modal-title fs-5" id="exampleModalLabel">
-                                                                    Modal title</h1>
+                                                                    Send Your Review pdf</h1>
                                                                 <button type="button" class="btn-close"
                                                                     data-bs-dismiss="modal" aria-label="Close"></button>
                                                             </div>
@@ -74,11 +74,12 @@
                                                                 <input type="file" class="form-control"
                                                                     wire:model="pdf" accept="application/pdf">
                                                             </div>
+                                                            <div wire:loading wire:target="pdf" class="text-center text-success">{{__('Uploading...')}}</div>
                                                             <div class="modal-footer">
                                                                 <button type="button" class="btn btn-secondary"
                                                                     data-bs-dismiss="modal">Close</button>
                                                                 <button type="button" class="btn btn-primary"
-                                                                    wire:click.prevent="submit({{ $data->accounts_need_review_id }})">Save
+                                                                    wire:click.prevent="submit({{ $data->accounts_need_review_id }})" wire:loading.remove wire:target="pdf">Save
                                                                     changes</button>
                                                             </div>
                                                         </div>
